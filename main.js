4b91dc45d6c86ab54422d2333c340db356a47573
Zyad <zyad.yasser@nyu.edu>
2021-09-14 18:26:06 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+// Get the video element
+const video = document.querySelector('#video')
+// Check if device has camera
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  // Use video without audio
+  const constraints = { 
+    video: true,
+    audio: false
+  }
+  
+  // Start video stream
+  navigator.mediaDevices.getUserMedia(constraints).then(stream => video.srcObject = stream);
+}
+
+
+
+// Create new barcode detector
+const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
+
+// Detect code function 
+const detectCode = () => {
+  // Start detecting codes on to the video element
+  barcodeDetector.detect(video).then(codes => {
+    if (codes.length === 0) return;
+    
+    console.log(codes[0].rawValue);
+  });
+}
+
+setInterval(detectCode, 100);
+
